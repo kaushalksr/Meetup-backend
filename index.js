fa34f1eb452a6da654b65c57cf0034f99d562dfa
KAUSHAL_KISHORE <66373966+kaushalksr@users.noreply.github.com>
2025-12-02 15:13:37 +0530

    feature:adding api to get meetup events

---
+++ index.js
@@ -0,0 +1,68 @@
+const { initializeDatabase } = require("./db/db.connect");
+const express = require("express");
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const Event = require("./models/event.model");
+
+initializeDatabase();
+
+// TO ADD NEW EVENT
+
+async function addNewEvent(newEvent) {
+  try {
+    const event = new Event(newEvent);
+    const savedEvent = await event.save();
+    return savedEvent;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.post("/events", async (req, res) => {
+  try {
+    const event = await addNewEvent(req.body);
+    if (event) {
+      res.json(event);
+    } else {
+      res.status(404).json({ message: "Failed to add new event!" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error ocurred while adding new event!" });
+  }
+});
+
+// TO READ ALL EVENTS
+
+async function readAllEvents() {
+  try {
+    const allEvents = await Event.find();
+    return allEvents;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.get("/events", async (req, res) => {
+  try {
+    const allEvents = await readAllEvents();
+    if (allEvents) {
+      res.json(allEvents);
+    } else {
+      res.status(404).json({ message: "Failed to fetch event!" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error ocurred while fetching event!" });
+  }
+});
+
+PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
---
+++ models/event.model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    eventTitle: {
+      type: String,
+      required: true,
+    },
+    eventDate: {
+      type: Date,
+      required: true,
+    },
+    eventTime: {
+      type: String,
+      required: true,
+    },
+    eventType: {
+      type: String,
+      required: true,
+      enum: ["Offline", "Online"],
+    },
+    eventImage: {
+      type: String,
+      required: true,
+    },
+    hostedBy: {
+      type: String,
+      required: true,
+    },
+    eventDescription: {
+      type: String,
+      required: true,
+    },
+    eventDressCode: String,
+    eventAge: String,
+    eventTags: [
+      {
+        type: String,
+      },
+    ],
+    speakerName: String,
+    speakerImageUrl: String,
+    speakerDesignation: String,
+    eventPricing: Number,
+    eventAddress: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
